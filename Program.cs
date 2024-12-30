a69eaf9393d0736f6058c3a124ed4312dd8a6dc8
scg984261 <scottgillen@hotmail.com>
2024-12-30 10:16:24 +0000

    First commit starting new project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomerManagementSystem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, from CMS!");
+        }
+    }
+}
