b6538f6de8775be43b972b43edc66c6bf5cea59f
scg984261 <scottgillen@hotmail.com>
2025-01-19 12:37:21 +0000

    Header bar and menu bar

---
+++ CustomerManagementSystem/View/UserControls/FooterBar.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Controls;
+
+namespace CustomerManagementSystem.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for FooterBar.xaml
+    /// </summary>
+    public partial class FooterBar : UserControl
+    {
+        public string MachineName
+        { 
+            get
+            {
+                return Environment.MachineName;
+            }
+        }
+
+        public string VersionNumber
+        {
+            get
+            {
+                return Assembly.GetAssembly(this.GetType()).GetName().Version.ToString();
+            }
+        }
+
+        public FooterBar()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
